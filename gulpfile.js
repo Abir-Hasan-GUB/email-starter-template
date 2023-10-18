57e1a664bc9f8582447d3b8c6c692d8e1165e203
abir-hasan-gub <abirhasan6111@gmail.com>
2023-10-19 05:27:58 +0600

    Ready for lts nodJs

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,26 @@
-// Please flow this steps
-// 1. install npm
-// 2. npm install gulp -g
-// 3. npm init
-// 4. npm install gulp -D
-// 5. npm install gulp-sass
-// 6. npm install bootstrap jquery popper.js @fortawesome/fontawesome-free --save
-// 7. npm install browser-sync
-
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 
 // Compile SASS
-gulp.task('sass', function(){
+function compileSass() {
   return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-    .pipe(sass())
-    .pipe(gulp.dest("src/css"))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
-});
+}
 
-gulp.task('watch', function(){
-    browserSync.init({
-        server: {
-          baseDir: './src'
-        }
-      });
-      gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series(['sass']));
-      gulp.watch("src/*.html").on('change', browserSync.reload);
-      return
+// Watch for changes
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    }
   });
 
-gulp.task('default', gulp.series(['sass', 'watch']));
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], compileSass);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+}
+
+// Define the default task
+gulp.task('default', gulp.series(compileSass, watch));
